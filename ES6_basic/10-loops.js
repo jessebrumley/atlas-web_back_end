33345b5be96c34b9013551bfd83e3e453fae5a10
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-07 15:38:08 -0500

    checker issues

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [...array];
 
-  for (const value of newArray) {
+  for (const value of array) {
     newArray[newArray.indexOf(value)] = appendString + value;
   }
 
