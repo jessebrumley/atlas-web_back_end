52e6e4634f5293da2fa0483ac9f2fd25f02fe7cc
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-07 15:55:43 -0500

    i am lost

---
+++ ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = {};
+
+  for (const [department, employees] of Object.entries(employeesList)) {
+    allEmployees[department] = employees;
+  }
+
+  return {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(employeesList).length;
+    }
+  };
+}
