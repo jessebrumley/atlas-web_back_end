5e2a20961f035ab6c965d5d04ae6e875022a4bbb
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-08 15:28:12 -0500

    5a

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  constructor(sqft) {
+    if (this.constructor !== Building) {
+      if (!(this.evacuationWarningMessage)) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+    this.sqft = sqft;
+  }
+
+  set sqft(val) {
+    if (typeof (val) === 'number') {
+      this._sqft = val;
+    } else {
+      throw TypeError('sqft must be a number');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
