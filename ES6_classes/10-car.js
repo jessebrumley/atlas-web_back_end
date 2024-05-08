e7e16f99e430d6d4412a6b03bc47e18280a1f61c
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-08 18:38:14 -0500

    10 test

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,12 +1,35 @@
 export default class Car {
-
   constructor(brand, motor, color) {
-  this.brand = brand;
-  this.motor = motor;
-  this.color = color
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
   }
 
-  cloneCar {
-  return new Car(this.brand, this.motor, this.color);
+  get brand() {
+    return this.brand;
+  }
+
+  get motor() {
+    return this.motor;
+  }
+
+  get color() {
+    return this.color;
+  }
+
+  set brand(newBrand) {
+    this._brand = newBrand;
+  }
+
+  set motor(newMotor) {
+    this._motor = newMotor;
+  }
+
+  set color(newColor) {
+    this._color = newColor;
+  }
+
+  cloneCar() {
+    return new Car(this.brand, this.motor, this.color);
   }
 }
