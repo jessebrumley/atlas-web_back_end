efde5b0f40bf65ddcf699cd3169437ba7bf8fe76
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-08 15:09:54 -0500

    4f test4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,6 +1,6 @@
 import Currency from './3-currency';
 
-export default class Pricing {
+export default class Pricing (amount, currency){
   constructor(amount, currency) {
     this._amount = amount;
     this._currency = currency;
