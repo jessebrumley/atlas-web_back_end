b18d8915ea342e42df13923ab2d1de7e9aeeabd4
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-08 18:06:01 -0500

    7,8

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,35 @@
+export default class Airport {
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+
+  get name() {
+    return this._name;
+  }
+  
+  set name(val) {
+    if (typeof (val) === 'string') {
+      this._name = val;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+  
+  set code(val) {
+    if (typeof (val) === 'string') {
+      this._code = val;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+}
---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,38 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  valueOf() {
+    return this.size;
+  }
+
+  toString() {
+    return this.location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(value) {
+    if (typeof (value) === 'number' || value >= 0) {
+      this._size = value;
+    } else {
+      throw TypeError('Size must be a positive number');
+    }
+  }
+
+  get location() {
+    return this.location;
+  }
+
+  set location(newLocation) {
+    if (typeof (newLocation) === 'string') {
+      this._location = newLocation;
+    } else {
+      throw TypeError('Location must be a string');
+    }
+  }
+}
