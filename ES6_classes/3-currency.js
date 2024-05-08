49c8fb32760a564f92a069393f5e3ca525ac3e6b
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-08 14:09:06 -0500

    3a

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,34 @@
+export default class Currency {
+  constructor(code, name) {
+    this._name = name;
+    this._code = code;
+  }
+
+  set name(val) {
+    if (typeof (val) === 'string') {
+      this._name = val;
+    } else {
+      throw TypeError('Name must be a string');
+    }
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set code(val) {
+    if (typeof (val) === 'string') {
+      this._code = val;
+    } else {
+      throw TypeError('Code must be a string');
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  displayFullCurrency() {
+    return `${this.name} (${this.code})`;
+  }
+}
