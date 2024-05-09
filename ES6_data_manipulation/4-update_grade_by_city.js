1240738cc673ad5e4ecae8de15899f345c7534b2
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-05-08 19:45:04 -0500

    4b fixed

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,10 @@
 export default function updateStudentGradeByCity(array, city, newGrades) {
-  const filteredStudents = array.filter(student => student.location === city);
+  const filteredStudents = array.filter((student) => student.location === city);
 
-  const gradedStudents = filteredStudents.map(student => {
-    const studentGrade = newGrades.find(grade => grade.studentId === student.id);
-    
-    return {...student, grade: studentGrade? studentGrade.grade : 'N/A' };
+  const gradedStudents = filteredStudents.map((student) => {
+    const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+
+    return { ...student, grade: studentGrade ? studentGrade.grade : 'N/A' };
   });
 
   return gradedStudents;
