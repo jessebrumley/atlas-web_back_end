0f828826fcfb19d7caef1b3e742b19ff906d5c3e
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-08-29 14:14:52 -0500

    sum

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module takes a list input_list of floats as argument and returns their sum as a float....
+"""
+import math
+
+def sum_list(input_list: list) -> return:float
+    """
+    Returns sum of input_list.
+
+    Args:
+      input_list: a list of float numbers
+    
+    Returns:
+        float: a single float number
+    """
+    sum(input_list)
