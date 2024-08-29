56575f640d55b7810a1f002bfa3cc09afad73752
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-08-29 02:48:53 -0500

    1-concat

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module contains the add function that takes two strings
+and concatenates them.
+"""
+
+def concat(str1: string, str2: string) -> string:
+    """
+    Returns a concatenated string.
+
+    Args:
+        str1 (string): The first string.
+        str2 (string): The second String.
+
+    Returns:
+        string: A combination of two strings.
+    """
+    return str1 + str2
