804513b2ffe932187e65f38e13cca99588ce1413
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-08-29 14:00:51 -0500

    test

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -3,20 +3,7 @@
 This module defines variables and returns them.
 """
 
-
-def func() -> return: tuple[int, float, bool, str]:
-    """
-    Returns static variables.
-
-    Args:
-
-    Returns:
-        tuple: A tuple containing an integer, a float, a boolean, and a string.
-    """
-
-    a: int = 1
-    pi: float = 3.14
-    i_understand_annotations: bool = True
-    school: str = "Holberton"
-
-    return a, pi, i_understand_annotations, school
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
