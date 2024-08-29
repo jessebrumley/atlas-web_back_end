decbbea49cb5dc0b4e84601044637a6fc04704a9
Jessterr <44988723+Jessterr@users.noreply.github.com>
2024-08-29 11:51:52 -0500

    wrote 2-floor, 9 is temp storing a template

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import math
+
+"""
+This module takes in a float and returns the largest integer
+less than or equal to the input
+"""
+
+def floor(n:float) -> int:
+    """
+    Returns the largest integer less than or equal to input.
+
+    Args:
+      n(float): A float
+    Returns:
+        int: largest int <= input
+    """
+    return math.floor(n)
---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+This module ...
+"""
+
+def func(input:type  input:type ) -> return_type:
+    """
+    Returns ...
+
+    Args:
+    
+    Returns:
+        :
+    """
