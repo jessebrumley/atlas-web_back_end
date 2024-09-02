9e984814d41a8acfe39f689a3117fcda75f1cdda
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-01 23:25:41 -0500

    testing Generator again

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -8,7 +8,8 @@ import random
 import asyncio
 from typing import Generator
 
-async def async_generator() -> typing.Generator[float, NoneType, NoneType]:
+
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields 10 random floating-point numbers
     between 0 and 10, each after waiting for 1 second.
@@ -19,4 +20,3 @@ async def async_generator() -> typing.Generator[float, NoneType, NoneType]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-
