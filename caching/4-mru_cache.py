a776886a5dbc4e25675ba4857bc32188ea51914d
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-17 17:40:12 -0500

    mru

---
+++ caching/4-mru_cache.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+""" MRUCache implements a Most Recently Used (MRU) caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache implements a Most Recently Used (MRU) caching system
+    """
+
+    def __init__(self):
+        """ Initializes the MRUCache instance
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """ Adds an item to the cache
+
+            Args:
+            - key: Key for the item
+            - item: Item to be added to the cache
+
+            Returns:
+            - None
+        """
+        if key is None or item is None:
+            return
+
+        # Remove the most recently used item if the cache is full
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            most_recent_key, _ = self.cache_data.popitem(last=True)
+            print(f"DISCARD: {most_recent_key}")
+
+        # Add the new item to the cache
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieves an item from the cache
+
+            Args:
+            - key: Key for the item
+
+            Returns:
+            - The item associated with the key, or None if key doesn't exist
+        """
+        if key is None:
+            return None
+
+        return self.cache_data.get(key)
