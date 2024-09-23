94c2f6fe42cc15dda186a49a0ebc88c4bdd55a50
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-22 19:39:38 -0500

    added logger function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -44,9 +44,39 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.FIELDS = fields
 
+    # Format the log record, filtering specified fields
     def format(self, record: logging.LogRecord) -> str:
         """ Formats the log record, filtering specified fields.
         """
         record.msg = filter_datum(self.FIELDS, self.REDACTION, record.msg,
                                   self.SEPARATOR)
         return super().format(record)
+
+
+# Create a constant tuple for PII fields
+PII_FIELDS = (
+    'name',
+    'email',
+    'phone',
+    'ssn',
+    'password'
+)
+
+
+# Create a logger object for user_data
+def get_logger() -> logging.Logger:
+    """ Creates and returns a logger object for user data.
+    """
+    # Initialize logger
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+
+    # Attach handler to logger
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+
+    # Prevent propagation to other loggers
+    logger.propagate = False
+
+    return logger
