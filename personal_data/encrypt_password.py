93fae920602cf1dadcf5b560dbd593f77a259e39
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-22 20:13:51 -0500

    added is_valid, uses bcrypt to compare input and hashed passwords

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 hash_password module will use bcrypt hashing to encrypt passwords.
+is_valid uses bcrypt to check is hashed password and input password match.
 """
 
 import bcrypt
@@ -20,3 +21,16 @@ def hash_password(password: str) -> bytes:
     # Hash the password
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ Validates a password against a hashed password.
+
+    Arguments:
+    hashed_password: The hashed password as a byte string.
+    password: The plain text password to validate.
+
+    Returns:
+    True if the password matches the hashed password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
