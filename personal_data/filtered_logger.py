b00b53f5ab92726be3e7558d095c3cfefce5721c
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-22 19:22:07 -0500

    changed init to accept string, changed format to record and return record.msg

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,6 +5,7 @@ filtered_logger module for obfuscating log messages.
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(
@@ -27,3 +28,25 @@ def filter_datum(
 
     return re.sub(pattern,
                   lambda match: f"{match.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """ Initializes the formatter with specified fields to redact.
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.FIELDS = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the log record, filtering specified fields.
+        """
+        record.msg = filter_datum(self.FIELDS, self.REDACTION, record.msg,
+                                  self.SEPARATOR)
+        return super().format(record)
