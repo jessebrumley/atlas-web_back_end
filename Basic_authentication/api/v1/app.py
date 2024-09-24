d0c0d454bd4084b319cf3c772cd487d6c713bbbc
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-23 21:49:05 -0500

    updated before_request

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -3,19 +3,41 @@
 Route module for the API
 """
 from os import getenv
-from api.v1.views.index import app_views
+from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
-from api.v1.auth.auth import Auth
+from flask_cors import (CORS, cross_origin)
+import os
+auth = None
+auth = os.getenv('AUTH_TYPE')
+if auth == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
-auth_type = getenv("AUTH_TYPE")
-if auth_type == "Auth":
-    auth = Auth()
+
+@app.before_request
+def handle_request():
+    """ handle request authorization
+    """
+    print(request.path)
+    print(request.headers)
+    print(request.authorization)
+    handled_paths = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
+    if auth is not None:
+        if auth.require_auth(request.path, handled_paths) is True:
+            if auth.authorization_header(request) is None:
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
 
 
 @app.errorhandler(404)
@@ -26,40 +48,17 @@ def not_found(error) -> str:
 
 
 @app.errorhandler(401)
-def unauthorized(error) -> str:
-    """ Unauthorized handler
+def not_authorized(error) -> str:
+    """ Not authorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Forbidden handler
+def access_forbidden(error) -> str:
+    """ Forbidden route handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app.before_request
-def before_request() -> None:
-    """ Validates each request before processing
-    Raises:
-        401: If authorization header is missing
-        403: If current user is not authenticated
-    """
-    if auth is None:
-        return
-
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-
-    if not auth.require_auth(request.path, excluded_paths):
-        return
-
-    if auth.authorization_header(request) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 if __name__ == "__main__":
