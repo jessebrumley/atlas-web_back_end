dd0543237a0347ba0911e94c9e7499c37e3f9e45
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-23 19:46:05 -0500

    testing unauthorized error

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,8 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/api/v1/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    """ Endpoint to raise a 401 Unauthorized error. """
+    abort(401)
