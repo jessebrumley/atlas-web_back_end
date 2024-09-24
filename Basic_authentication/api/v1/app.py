feca520f9c08cd575fc4832a4fc93dfb5e15cfba
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-23 22:18:00 -0500

    revering to basic_auth commit

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """
-Route module for the API.
+Route module for the API
 """
-
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
@@ -19,21 +18,19 @@ else:
     auth = Auth()
 
 app = Flask(__name__)
-
-# Register the blueprint here to ensure it's only done once
 app.register_blueprint(app_views)
-
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.before_request
 def handle_request():
-    """Handle request authorization."""
-    handled_paths = ['/api/v1/status',
-                     '/api/v1/unauthorized',
-                     '/api/v1/forbidden']
+    """ Handle request authorization
+    """
+    handled_paths = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/']
     if auth is not None:
-        if auth.require_auth(request.path, handled_paths):
+        if auth.require_auth(request.path, handled_paths) is True:
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
@@ -42,51 +39,23 @@ def handle_request():
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        A JSON response with a 404 status.
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def not_authorized(error) -> str:
-    """Not authorized handler.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        A JSON response with a 401 status.
+    """ Not authorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def access_forbidden(error) -> str:
-    """Forbidden route handler.
-
-    Args:
-        error: The error object.
-
-    Returns:
-        A JSON response with a 403 status.
+    """ Forbidden route handler
     """
     return jsonify({"error": "Forbidden"}), 403
-
-
-@app_views.route('/api/v1/status', methods=['GET'])
-def status():
-    """Returns the status of the API.
-
-    Returns:
-        A JSON response with the status of the API.
-    """
-    return jsonify({"status": "OK"}), 200
 
 
 if __name__ == "__main__":
