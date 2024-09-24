7bfef350e51eb9d2b83869e7ab0e357778d01003
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-23 20:48:11 -0500

    reverting to previous

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,75 +1,27 @@
 #!/usr/bin/env python3
 """
-Functions:
-- before_request_handler(): Checks authorization before requests.
-- not_found(error): Returns a 404 error message.
-- unauthorized(error): Returns a 401 error message.
-- forbidden(error): Returns a 403 error message.
+Route module for the API
 """
 from os import getenv
-from api.v1.auth.auth import Auth
 from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
+from flask import Flask, jsonify, abort
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
-
-auth_type = os.getenv('AUTH_TYPE')
-
-if auth_type == "basic_auth":
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
-    auth = Auth()
-
-
-@app.before_request
-def before_request_handler():
-    """ Authorizes requests before processing.
-    """
-    if auth is None:
-        return
-
-    excluded_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-
-    if request.path in excluded_paths:
-        return
-
-    if request.path not in excluded_paths and \
-            auth.require_auth(request.path, excluded_paths):
-        if auth.authorization_header(request) is None:
-            abort(401)
-
-
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Handles 404 errors.
+    """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
-
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Handles 401 errors.
+    """ Unauthorized
     """
     return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """ Handles 403 errors.
-    """
-    return jsonify({"error": "Forbidden"}), 403
-
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,40 +1,30 @@
 #!/usr/bin/env python3
-""" Index Views Module
+""" Module of Index views
 """
-from flask import jsonify, abort
-from api.v1.views import app_views
+from flask import jsonify, abort, Blueprint
 
+app_views = Blueprint('app_views', __name__)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ Endpoint to check API status
-    Returns:
-      - A JSON object with the API's status.
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
     """
     return jsonify({"status": "OK"})
 
-
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized():
-    """ Route for unauthorized access
-    """
-    abort(401)
-
-
-@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
-def forbidden_route():
-    """ Route for forbidden access
-    """
-    abort(403)
-
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ Endpoint to retrieve statistics
-    Returns:
-      - A JSON object containing counts of various objects.
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized_endpoint():
+    """ Endpoint to raise a 401 Unauthorized error. """
+    abort(401)
