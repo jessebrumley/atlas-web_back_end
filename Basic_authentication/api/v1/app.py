db4c18fe6b8741f46fb2102282464319ee20ca29
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-23 20:43:10 -0500

    moving on to next task for forbidden..

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -1,57 +1,74 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+Functions:
+- before_request_handler(): Checks authorization before requests.
+- not_found(error): Returns a 404 error message.
+- unauthorized(error): Returns a 401 error message.
+- forbidden(error): Returns a 403 error message.
 """
 from os import getenv
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
 auth = None
-auth = os.getenv('AUTH_TYPE')
-if auth == 'basic_auth':
+
+auth_type = os.getenv('AUTH_TYPE')
+
+if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
-    from api.v1.auth.auth import Auth
     auth = Auth()
 
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
 @app.before_request
-def handle_request():
-    """ handle request authorization
+def before_request_handler():
+    """ Authorizes requests before processing.
     """
-    print(request.path)
-    print(request.headers)
-    print(request.authorization)
-    handled_paths = ['/api/v1/status/',
-                     '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
-    if auth is not None:
-        if auth.require_auth(request.path, handled_paths) is True:
-            if auth.authorization_header(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
+    if auth is None:
+        return
+
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    if request.path in excluded_paths:
+        return
+
+    if request.path not in excluded_paths and \
+            auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
 
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """ Handles 404 errors.
     """
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
-def not_authorized(error) -> str:
-    """ Not authorized handler
+def unauthorized(error) -> str:
+    """ Handles 401 errors.
     """
     return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Handles 403 errors.
+    """
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Module of Index views
+""" Index Views Module
 """
 from flask import jsonify, abort
 from api.v1.views import app_views
@@ -7,27 +7,34 @@ from api.v1.views import app_views
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
+    """ Endpoint to check API status
+    Returns:
+      - A JSON object with the API's status.
     """
     return jsonify({"status": "OK"})
 
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """ Route for unauthorized access
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
+def forbidden_route():
+    """ Route for forbidden access
+    """
+    abort(403)
+
+
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
+    """ Endpoint to retrieve statistics
+    Returns:
+      - A JSON object containing counts of various objects.
     """
     from models.user import User
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
-
-
-@app_views.route('/unauthorized', strict_slashes=False)
-def unauthorized():
-    """ Unauthorized endpoint
-    """
-    abort(401)
