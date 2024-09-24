9ef401da26cbf18de359f1b809b78fc58da334ff
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-09-23 22:02:33 -0500

    testing fix

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -24,13 +24,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.before_request
 def handle_request():
-    """ Handle request authorization
-    """
-    handled_paths = ['/api/v1/status/',
-                     '/api/v1/unauthorized/',
-                     '/api/v1/forbidden/']
+    """Handle request authorization."""
+    handled_paths = ['/api/v1/status',  # Removed trailing slash
+                     '/api/v1/unauthorized',
+                     '/api/v1/forbidden']
     if auth is not None:
-        if auth.require_auth(request.path, handled_paths) is True:
+        if auth.require_auth(request.path, handled_paths):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
@@ -39,22 +38,19 @@ def handle_request():
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """Not found handler."""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.errorhandler(401)
 def not_authorized(error) -> str:
-    """ Not authorized handler
-    """
+    """Not authorized handler."""
     return jsonify({"error": "Unauthorized"}), 401
 
 
 @app.errorhandler(403)
 def access_forbidden(error) -> str:
-    """ Forbidden route handler
-    """
+    """Forbidden route handler."""
     return jsonify({"error": "Forbidden"}), 403
 
 
