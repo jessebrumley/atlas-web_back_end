fecf5c0048447da44d97cf15f7923cdd340cb704
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-10-07 02:40:42 -0500

    added counter using redis.incr

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -2,6 +2,8 @@
 import redis
 import uuid
 from typing import Callable, Optional, Union
+import functools
+
 
 class Cache:
     def __init__(self) -> None:
@@ -55,3 +57,33 @@ class Cache:
     def get_int(self, key: str) -> Optional[int]:
         # Call get method and convert to integer
         return self.get(key, lambda x: int(x))
+    
+    def count_calls(method: Callable) -> Callable:
+        """
+        Counts the number of times a method is called
+
+        Args:
+            method: The method to check
+        
+        Returns:
+            The method that increments a call count in Redis.
+        """
+        @functools.wraps(method)
+        def wrapper(self, *args, **kwargs):
+            # Use the method's name to create a Redis key for counting
+            key = method.__qualname__
+            
+            # Increment the count for this method in Redis
+            self._redis.incr(key)
+            
+            # Call the original method and return its result
+            return method(self, *args, **kwargs)
+        
+        return wrapper
+
+    @count_calls  # Decorate the store method with count_calls
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """Stores data in Redis with a random key."""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
