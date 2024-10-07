ca4a137e0c8762ecc37e563114c18a0e64fa6b43
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-10-07 02:35:22 -0500

    added get methods

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import redis
 import uuid
-from typing import Union
+from typing import Callable, Optional, Union
 
 class Cache:
     def __init__(self) -> None:
@@ -22,3 +22,36 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None
+    ) -> Optional[Union[str, int, bytes]]:
+        """
+        Retrieve value from Redis and optionally converts to
+        integer or string format
+
+        Args:
+            data: the data to be stored
+        Returns:
+            the generated key as a string
+        """
+        # retrieve value from Redis
+        value = self._redis.get(key)
+        
+        # If no key, return none
+        if value is None:
+            return None
+        
+        # apply convert functionn
+        if fn is not None:
+            return fn(value)
+        
+        # return as is
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        # call get method and convert to string
+        return self.get(key, lambda x: x.decode('utf-8') if isinstance(x, bytes) else x)
+
+    def get_int(self, key: str) -> Optional[int]:
+        # Call get method and convert to integer
+        return self.get(key, lambda x: int(x))
