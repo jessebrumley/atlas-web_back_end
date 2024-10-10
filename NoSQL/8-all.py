c3f769c48c61a94ccfa8e54971a342bd9d18dc18
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2024-10-09 20:45:30 -0500

    module 8 and 9

---
+++ NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+""" lists all documents in a collection
+"""
+
+
+def list_all(mongo_collection):
+    """
+    lists all documents in a collection
+    Args:
+        mongo_collection: Mongo DB collection
+
+    Returns:
+       Empty list or all documents in collection
+    """
+    return mongo_collection.find()
