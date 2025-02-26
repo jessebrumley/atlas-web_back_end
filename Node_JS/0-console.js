eb98c63c0bd4a73f1ddbb6eaf7373d9887996fd2
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2025-02-25 23:37:23 -0600

    complete function that prints a string to STDOUT

---
+++ Node_JS/0-console.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+/**
+ * displayMessage - prints a string to STDOUT
+ * @param {string} message - the message to print
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
