b3e95d5eae4dd9a00d324072899fe4eab62262cf
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2025-03-04 01:43:33 -0600

    feat: add stub test for sendPaymentRequestToApi using Sinon

---
+++ unittests_in_js/4-payment.test.js
@@ -0,0 +1,25 @@
+const sinon = require('sinon');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const assert = require('assert');
+
+describe('sendPaymentRequestToApi', function () {
+    it('should always return 10 when calling Utils.calculateNumber', function () {
+        // Stub Utils.calculateNumber to always return 10
+        const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+        // Spy on console.log
+        const spyConsoleLog = sinon.spy(console, 'log');
+
+        sendPaymentRequestToApi(100, 20);
+
+        // Asserts that the stub was called with 100, 20
+        sinon.assert.calledOnceWithExactly(stub, 'SUM', 100, 20);
+
+        // Asserts that console.log displays stub result
+        assert(spyConsoleLog.calledOnceWith('The total is: 10'));
+
+        stub.restore();
+        spyConsoleLog.restore();
+    });
+});
