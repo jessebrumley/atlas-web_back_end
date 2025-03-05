887e848a07fa5bea2762c97db071e2f2e1d4dbea
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2025-03-05 16:03:35 -0600

    test: add async test for getPaymentTokenFromAPI

---
+++ unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', function () {
+    it('should return a resolved promise with correct data when success is true', function (done) {
+        getPaymentTokenFromAPI(true)
+            .then((response) => {
+                expect(response).to.deep.equal({ data: 'Successful response from the API' });
+                done();
+            })
+            .catch((error) => done(error));
+    });
+});
