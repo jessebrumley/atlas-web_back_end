3a3512495e0757a6fbb9c49e3dbd9358b6354eec
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2025-03-05 18:22:52 -0600

    feat: added express server api.js, testing completed

---
+++ unittests_in_js/8-api/api.test.js
@@ -0,0 +1,18 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('/', function () {
+  it('should return status code 200', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should return "Welcome to the payment system"', function (done) {
+    request('http://localhost:7865', function (error, response, body) {
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
