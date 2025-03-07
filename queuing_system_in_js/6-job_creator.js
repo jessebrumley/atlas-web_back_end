730480f030617680d40e08d451a6ee7334dee71a
Jessterr <44988723+Jessterr@users.noreply.github.com>
2025-03-07 00:00:18 -0600

    edited job creator

--- queuing_system_in_js/6-job_creator.js
+++ queuing_system_in_js/6-job_creator.js
@@ -6,12 +6,24 @@ const jobData = {
     message: 'This is a test notification',
 };
 
-const job = queue.create('push_notification_code', jobData).save((err) => {
-    if (!err) console.log(`Notification job created: ${job.id}`);
+const job = queue.create('push_notification_code', jobData);
+
+job.save((err) => {
+    if (err) {
+        console.error('Error saving job:', err);
+    } else {
+        console.log(`Notification job created: ${job.id}`);
+    }
 });
 
 job.on('complete', () => {
     console.log('Notification job completed');
-}).on('failed', () => {
-    console.log('Notification job failed');
+}).on('failed', (err) => {
+    console.log(`Notification job failed: ${err}`);
+});
+
+queue.process('push_notification_code', (job, done) => {
+    const { phoneNumber, message } = job.data;
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+    done();
 });
