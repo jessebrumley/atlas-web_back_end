09d05b01b8d5b15b0a523476f5a6b97e3368e71b
jessebrumley <155807330+jessebrumley@users.noreply.github.com>
2025-04-20 18:17:53 -0500

    added genres endpoint

--- custom-api-from-scratch/index.js
+++ custom-api-from-scratch/index.js
@@ -17,3 +17,15 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/genres', (req, res) => {
+    const genres = data.genres.map(({ id, name }) => ({ id, name }));
+    res.json(genres);
+  });
+  
+  app.get('/genres/:id', (req, res) => {
+    const genre = data.genres.find(g => g.id == req.params.id);
+    if (!genre) return res.status(404).json({ error: 'Genre not found' });
+    res.json(genre);
+  });
+  
